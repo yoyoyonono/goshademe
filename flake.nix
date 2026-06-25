71698c740deee5919f3005adbbdb4b577fe091eb
Aaron Thapa <notsmart98@gmail.com>
2026-06-24 20:57:04 -0400

    flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "goshademe - crowdfunding app";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+          ];
+
+          shellHook = ''
+            if [ ! -d node_modules ]; then
+              npm install
+            fi
+          '';
+        };
+      }
+    );
+}
